6d555fef09474d78bed5858df11e3ffa6a10668a
Giorgos Pontikakis <giorgos.pontikakis@gmail.com>
2009-08-23 15:32:13 +0300

    Added html-utils.lisp

---
+++ src/lisp/html-utils.lisp
@@ -0,0 +1,13 @@
+(in-package :scrooge)
+
+
+(defmacro with-auth (role &body body)
+  (with-gensyms (user)
+    `(let ((,user (session-value 'user)))
+       (cond ((null ,user)
+	      (redirect (login)))
+	     ((string-not-equal (webrole ,user) ,role)
+	      (redirect (unauthorized)))
+	     (t
+	      (progn
+		,@body))))))
