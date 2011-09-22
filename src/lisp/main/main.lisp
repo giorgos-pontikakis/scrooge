6ed9f6238c76c37da35b5e3d373c0830548bbb3b
Giorgos Pontikakis <giorgos.pontikakis@gmail.com>
2011-09-22 22:07:36 +0300

    Changed directory structure

--- src/lisp/main/main.lisp
+++ src/lisp/main/main.lisp
@@ -1,34 +1,25 @@
 (in-package :scrooge)
 
 
+;;; ----------------------------------------------------------------------
 ;;; UI elements
+;;; ----------------------------------------------------------------------
 
-(defun financial-navbar (active)
-  (navbar `((tx      ,(tx)                   "Συναλλαγές")
+(defun admin-navbar (&optional active)
+  (navbar `((company ,(company)              "Εταιρίες")
+            (project ,(project)              "Έργα")
+            (tx      ,(tx)                   "Συναλλαγές")
             (cash    ,(cash "revenue")       "Μετρητά")
             (invoice ,(invoice "receivable") "Τιμολόγια")
-            #|(cheque      ,(cheque "receivable")  "Επιταγές")|#
-            #|(account     ,(account/overview)     "Λογαριασμοί")|#)
+            (cheque  ,(cheque "receivable")  "Επιταγές"))
           :css-class "section-navbar hnavbar grid_12"
           :active active))
 
 
 
-;;; main page
-
-(defpage root-page financial ("financial/") ()
-  (with-view-page
-    (with-document ()
-      (:head
-       (:title "Οικονομικά")
-       (global-headers))
-      (:body
-       (:div :id "container" :class "container_12"
-             (header 'financial)
-             (financial-navbar nil)
-             (:div :id "content"
-                   (:p "Financial content not yet available")))))))
-
+;;; ----------------------------------------------------------------------
+;;; Widgets
+;;; ----------------------------------------------------------------------
 
 ;;; account-radio tree
 
