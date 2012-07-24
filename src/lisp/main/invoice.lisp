8b18cead5c0da5c9bc8d11ebfdf3a73288752b87
Giorgos Pontikakis <giorgos.pontikakis@gmail.com>
2012-07-25 00:32:28 +0300

    Changed customer/supplier labels for invoices (discarded incoming/outgoing in parentheses)

--- src/lisp/main/invoice.lisp
+++ src/lisp/main/invoice.lisp
@@ -150,9 +150,9 @@
         (kind (kind tbl))
         (filter (filter tbl)))
     (filter-area (filter-navbar `((incoming ,(apply #'invoice "incoming" "debit" filter)
-                                            "Πελάτες (Εισερχόμενα)")
+                                            "Πελάτες")
                                   (outgoing ,(apply #'invoice "outgoing" "credit" filter)
-                                            "Προμηθευτές (Εξερχόμενα)"))
+                                            "Προμηθευτές"))
                                 :active direction
                                 :id "direction-navbar")
                  (filter-navbar `((debit ,(apply #'invoice direction "debit" filter)
